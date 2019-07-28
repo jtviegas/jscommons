2962408c1e996ae8ed5c8d87e14b4e222593c1a6
joao tiago viegas <joao.viegas@maersk.com>
2019-07-28 14:51:18 +0200

    added a commons object with utility functions

--- index.js
+++ index.js
@@ -8,5 +8,46 @@ class ServerError extends Error {
     }
 }
 
+const commons = {
+
+    configByEnvironment: (config, variables, defaults, rangeSuffix, splitVariables) => {
+
+        for(let i in variables){
+
+            let variable = variables[i];
+            let defaultValue = defaults[i];
+
+            if ( process.env[variable] ){
+
+                let value = null;
+                if( splitVariables && 0 < splitVariables.length &&  -1 < splitVariables.indexOf(variable) ){
+                    let list = process.env[variable];
+                    value = list.split(',');
+                }
+                else
+                    value = process.env[variable];
+
+                let rangeVariable = variable + rangeSuffix;
+                if( config[rangeVariable] ){
+                    let range = config[rangeVariable];
+                    if( -1 === range.indexOf(value) )
+                        throw new Error('!!! variable: ' + variable + ' has an invalid value: ' + value + ' !!!');
+                }
+
+                config[variable] = value;
+            }
+            else
+                config[variable] = defaultValue;
+
+        }
+
+        return config;
+    }
+
+
+
+}
+
 module.exports = {};
+module.exports.commons = commons;
 module.exports.ServerError = ServerError;
