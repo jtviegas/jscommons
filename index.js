a5557d063abd048231adb034d8ac80e6f1e6aab8
joao tiago viegas <joao.viegas@maersk.com>
2019-08-01 22:37:25 +0200

    added configByDecoration method to commons

--- index.js
+++ index.js
@@ -43,6 +43,39 @@ const commons = {
 
         return config;
     }
+    , configByDecoration: (config, variables, defaults, decoration, rangeSuffix, splitVariables) => {
+
+        for(let i in variables){
+
+            let variable = variables[i];
+            let defaultValue = defaults[i];
+
+            if ( decoration[variable] ){
+
+                let value = null;
+                if( splitVariables && 0 < splitVariables.length &&  -1 < splitVariables.indexOf(variable) ){
+                    let list = decoration[variable];
+                    value = list.split(',');
+                }
+                else
+                    value = decoration[variable];
+
+                let rangeVariable = variable + rangeSuffix;
+                if( config[rangeVariable] ){
+                    let range = config[rangeVariable];
+                    if( -1 === range.indexOf(value) )
+                        throw new Error('!!! variable: ' + variable + ' has an invalid value: ' + value + ' !!!');
+                }
+
+                config[variable] = value;
+            }
+            else
+                config[variable] = defaultValue;
+
+        }
+
+        return config;
+    }
 
 
 
